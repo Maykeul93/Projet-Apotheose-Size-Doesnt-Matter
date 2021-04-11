cb0cd1bca88da2fbfa8fd35f24267877551f9aae
nmelling <nicolas.mellinger1@gmail.com>
2021-04-11 12:23:46 +0200

    Add static menu options + style/animation when user clic to display/remove menu

--- front/src/components/Menu/index.js
+++ front/src/components/Menu/index.js
@@ -2,8 +2,28 @@ import PropTypes from 'prop-types';
 import './style.scss';
 
 function Menu() {
+    const toggleMenu = () => {
+        console.log('j\'affiche le menu');
+        // Comportement à modifier une fois redux et les states implémentés
+        // Display menu since user click a first time on menu
+        document.querySelector('.menu__options').classList.add('is-displayed');
+        document.querySelector('.menu__options').classList.toggle('is-active');
+    };
+
     return (
-        <div className="menu">Menu</div>
+        <div className="menu">
+            <div
+                className="menu__icon"
+                onClick={toggleMenu}
+            >Menu</div>
+            <ul
+                className="menu__options"
+            >
+                <li className="menu__option">Mon profil</li>
+                <li className="menu__option">Jouer</li>
+                <li className="menu__option">Se déconnecter</li>
+            </ul>
+        </div>
     );
 }
 
