699b4cad8d8db454fe3754fce3c6d3be9402d7f4
nmelling <nicolas.mellinger1@gmail.com>
2021-04-11 11:42:34 +0200

    add special font into vars.scss & use it into header

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -7,7 +7,7 @@ import './style.scss';
 function Header() {
     return (
         <div className="header">
-            <div className="logo">Logo</div>
+            <div className="header__logo"><span>Logo</span></div>
             <h1 className="header__title">Size Doesn't Matter</h1>
             <Menu />
         </div>
