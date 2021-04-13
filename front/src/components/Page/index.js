7f652729377039d268bb4e77ccc8765b57fcd39a
nmelling <nicolas.mellinger1@gmail.com>
2021-04-13 18:34:31 +0200

    tree structure of Router + dynamically displayed of Content into Page Component

--- front/src/components/Page/index.js
+++ front/src/components/Page/index.js
@@ -1,3 +1,4 @@
+import { useParams, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Header from 'components/Header';
@@ -6,10 +7,44 @@ import Footer from 'components/Footer';
 import './style.scss';
 
 function Page() {
+    // get the params of the road to choose which content is displayed
+    const { params } = useParams();
+
+    console.log(params);
+
+    const component = {
+        signin: 'Signin Component',
+        profil: 'Profil Component',
+        admin: 'Admin Component',
+        createRoom: 'CreateRoom Component',
+        aboutUs: 'AboutUs Component',
+        error: 'Error Component',
+    };
+
+    // Error gestion in case of road asked isn't exist
+    if (params) {
+        const isRoadExist = Object.keys(component).find((road) => road === params);
+
+        // If isRoadExist is undefined, redirect to Error Page
+        if (!isRoadExist) {
+            console.log('isRoadExist', isRoadExist);
+            return (<Redirect to="/error" />);
+        }
+    }
+
     return (
         <div className="page">
             <Header />
-            <div className="page__main">Page</div>
+            {/* By default, Home component will be displayed here.
+            Otherwise, looking for same key in component object and render the component  */}
+            {
+                params ? (
+                    <div>{component[params]}</div>
+                ) : (
+                    <div className="page__main">Home</div>
+                )
+            }
+            
             <Footer />
         </div>
     );
