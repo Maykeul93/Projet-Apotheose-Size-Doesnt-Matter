5520e756046240e3171eb03a7ab503a33437b0ec
nmelling <nicolas.mellinger1@gmail.com>
2021-04-14 07:49:54 +0200

    add Link to Home to header logo

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Menu from 'components/Menu';
@@ -7,7 +8,9 @@ import './style.scss';
 function Header() {
     return (
         <div className="header">
-            <div className="header__logo"><span>Logo</span></div>
+            <Link to="/">
+                <div className="header__logo"><span>Logo</span></div>
+            </Link>
             <h1 className="header__title">Size Doesn't Matter</h1>
             <Menu />
         </div>
