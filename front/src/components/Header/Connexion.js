b8711b157a61a3cfef5269cedca49a59f914c1a0
Maykeul93 <michael.mittoo@gmail.com>
2021-04-14 13:25:28 +0200

    Add Connexion Component v1

--- front/src/components/Header/Connexion.js
+++ front/src/components/Header/Connexion.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 
 const Connexion = () => {
+    const [displayed, setDisplayed] = useState(false);
+
+    const toggleDisplayed = () => {
+        setDisplayed(!displayed)
+    }
     return(
         <div className="connexion">
             <button 
             className="connexion__button" 
             type ="button"
-            onClick={() => console.log("connexion click")}
+            onClick={toggleDisplayed}
             >
                 Connexion
             </button>
-            <div className="connection__menu">
-                <input type="email" placeholder="email" />
-                <input type="password" placeholder="password" />
+            <div className={
+                classNames("connection__menu", {"is-displayed": displayed})
+            }>
+                <input className="connection__input" type="email" placeholder="email" />
+                <input className="connection__input" type="password" placeholder="password" />
                 <button type="submit">Valider</button>
             </div>
         </div>
