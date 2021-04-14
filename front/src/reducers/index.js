d134313aa1842f632b1d36ed1dbbd6fac0f1028c
nmelling <nicolas.mellinger1@gmail.com>
2021-04-14 07:59:24 +0200

    create game Reducer

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 
 // Import our other reducers
 import userReducer from './user';
+import gameReducer from './game';
 
 const rootReducer = combineReducers({
     user: userReducer,
+    game: gameReducer,
 });
 
 export default rootReducer;
