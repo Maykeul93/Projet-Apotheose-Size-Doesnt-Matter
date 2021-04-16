f5329862255c465d1c013fac6930974da9a22296
ArnaudG19 <arnaud.gueresse@gmail.com>
2021-04-16 12:13:06 +0200

    Modification socket's road

--- back/index.js
+++ back/index.js
@@ -11,6 +11,8 @@ const io = require('socket.io')(server, {
 }); 
 const cors = require('cors');
 
+const uniqid = require('uniqid'); 
+
 const bodyParser = require('body-parser'); //parse body's answer for token 
 
 const routers = require('./routers');
@@ -30,12 +32,50 @@ app.use(cors({
 
 app.use(routers); 
 
+
 io.on('connection', (socket) => {
-    console.log('a user connected'); 
+    console.log('a user connected :', socket.id); 
+    socket.on('front_create_game', (id) => {
+      const room = uniqid();
+      socket.join(room);
+      // requete SQL pour créer un enregistrement dans Game et y stocker la room
+      // utilisation de l'id de l'utilisateur pour le lier via la table de liaison à la nouvelle partie
+      // Si une erreur survient, envoyer l'erreur (bloc if pour envoyer l'event custom correspondant)
+      socket.emit('server_create_game', {
+        room,
+        // id de la partie (peut etre)
+      });
+
+      // socket.emit('server_create_game_error', {
+      //   error: sendError
+      // });
+    });
+
+    socket.on('front_join_game', ({ id, room }) => {
+      // Check in the database if code Room exists
+
+      // if exists, connect the player to the room
+      // Connect the player to the game into the Database
+      // if (exist) {
+        socket.on('server_join_game', {
+          room,
+          // id de la partie (peut etre)
+        });
+      // }
+      // if doesn't exist, send an error to the front
+      // else {
+        socket.on('server_join_game_error', {
+          // error: sendError
+        });
+      // }
+    });
+
     socket.on('disconnect', () => {
       console.log('user disconnect'); 
-    })
+    });
 });
+
+
 
 server.listen(process.env.PORT || 3000, () => {
   console.log('Server running on:', process.env.PORT);
