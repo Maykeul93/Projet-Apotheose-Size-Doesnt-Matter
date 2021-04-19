f5f857c1568c0a9e293a7e08569a2279eb62d8a7
ArnaudG19 <arnaud.gueresse@gmail.com>
2021-04-19 14:32:12 +0200

    Addind function NOW() to the date in user_play_game + COMMENTS SCOKET index.js

--- back/index.js
+++ back/index.js
@@ -34,15 +34,17 @@ app.use(routers);
 
 const gameController = require('./controllers/gameController'); 
 
+//Scoket Connection
 io.on('connection', (socket) => {
     console.log('a user connected :', socket.id); 
     socket.on('front_create_game', (idUser) => {
       const room = uniqid();
       socket.join(room);
-      // requete SQL pour créer un enregistrement dans Game et y stocker la room
+      // Insert the romm's number to the table game (room)
       const idGame = gameController.gameRecRoom(socket, room, idUser); 
     });
 
+    //Sending to the room all players (id and pseudo)
     socket.on('front_join_game', async ({ id, room }) => {
       // Check in the database if code Room exists
       //! Gestion d'erreur a revoir
@@ -65,16 +67,12 @@ io.on('connection', (socket) => {
       //ajouter avatar pour plus tard
     });
 
+    // Sending five random questions to the room
     socket.on('front_launch_game', async ({ id, room }) => {
-      // Recevra id de la game en plus
-      // cosnt questions = Requete SQL pour récupérer les questions
-      // Lier les question à la game
       const idGame = await gameController.checkRoom(room);
       if (idGame) {
         const questions = await gameController.sendRandomQuestion(); 
-        console.log(id, ' lance la partie!');
-        console.log(questions) ; 
-        io.to(room).emit('server_launch_game', {questions});
+        io.to(room).emit('server_launch_game', {questions, idGame});
       } else {
         socket.emit('server_join_game_error', {
           error: 'Cette partie n\'existe pas!!',
@@ -89,12 +87,25 @@ io.on('connection', (socket) => {
       });
     });
 
+    //Sendind the id for the player who clicked to the button 'quit the game'
     socket.on('front_leave_game', ({ id, room }) => {
       io.to(room).emit('server_leave_game', {
         id
       });
+      socket.leave(room); 
     });
 
+    //! features 
+    socket.on('front_send_score', ({room, userID, gameID, score, position, exactAnswer}) => {
+      // recup gameID en back 
+      //call controler to execute the datamapper function 
+      //emit 
+      io.to(room).emit('server_end_game', {
+        succes: "true" 
+      }); 
+    }); 
+
+    //Disconnection 
     socket.on('disconnect', () => {
       console.log('user disconnect'); 
     });
