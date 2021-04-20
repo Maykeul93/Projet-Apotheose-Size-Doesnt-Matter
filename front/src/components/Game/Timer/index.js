c92f6b4b02da65cf2eb9d6b439e5bd4067fd7f18
nmelling <nicolas.mellinger1@gmail.com>
2021-04-20 11:49:57 +0200

    trying to update local state in once

--- front/src/components/Game/Timer/index.js
+++ front/src/components/Game/Timer/index.js
@@ -3,17 +3,20 @@ import PropTypes from 'prop-types';
 import './style.scss';
 
 function Timmer({
-    isRound,
+    allValues,
     setRound,
-    isLaunch,
     startTimer,
 }) {
+    const { isRound } = allValues;
     const [seconds, setSeconds] = useState(isRound ? 30 : 15);
     
     useEffect(() => {
         if (startTimer) {
             if (seconds === 0) {
-                setRound(!isRound);
+                setRound({
+                    ...allValues,
+                    isRound: !isRound
+                });
             }
             const timer = 
             seconds > 0 && setInterval(() => setSeconds(seconds - 1), 1000);
@@ -33,7 +36,7 @@ function Timmer({
 }
 
 Timmer.propTypes = {
-    isRound: PropTypes.bool.isRequired,
+    allValues: PropTypes.object.isRequired,
     setRound: PropTypes.func.isRequired,
     isLaunch: PropTypes.bool.isRequired,
     startTimer: PropTypes.bool.isRequired,
