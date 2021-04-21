0b2a197b6fef596485caeefff13daf45652cf291
ArnaudG19 <arnaud.gueresse@gmail.com>
2021-04-21 16:25:49 +0200

    Modification JWT condition undefinded send 403 + Adding devStart for run nodemon index.js

--- back/utils/jwt.js
+++ back/utils/jwt.js
@@ -15,27 +15,13 @@ module.exports = {
     })
   },
   //Check to make sure header is not undefined, if so, return Forbidden (403)
-  checkToken (req, res, next) {
-    const header = req.headers['authorization']; 
-    console.log(header); 
-    if(typeof header !== 'undefined') {
-        const bearer = header.split(' ');
-        const token = bearer[1];
-        //il faudra peut être envoyer le req.body.token pour être ok si error
-        console.log(token); 
-        req.token = token;
-        next();
-    } else {
-        //If header is undefined return Forbidden (403)
-        res.sendStatus(403);
-    }
-  },
   authentificationToken (req, res, next) {
     const header = req.headers['authorization'];
     console.log(header); 
     const token = header && header.split(' ')[1];
     req.token = token;  
-    if (token == null) {
+    console.log(req.token); 
+    if (token == 'undefined') {
       return res.status(403); 
     }
     jwt.verify(req.token, process.env.JWT_SIGN_SECRET, (error) => {
