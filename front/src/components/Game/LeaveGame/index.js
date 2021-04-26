283c742efd6f551306e78c89207eb7d025f820e8
nmelling <nicolas.mellinger1@gmail.com>
2021-04-26 17:01:54 +0200

    add button to copy link in Room Component

--- front/src/components/Game/LeaveGame/index.js
+++ front/src/components/Game/LeaveGame/index.js
@@ -2,17 +2,23 @@ import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './style.scss';
 
-function LeaveGame({ leaveGame, buttonContent }) {
+function LeaveGame({ leaveGame, buttonContent, width }) {
     let history = useHistory();
     const handleClick = () => {
         history.push('/page/createRoom');
         leaveGame();
     };
+
+    const buttonStyles = {
+        width,
+    };
+
     return (
         <button
             className="leaveGame"
             type="button"
             onClick={handleClick}
+            style={buttonStyles}
         >
             {buttonContent}
         </button>
@@ -22,6 +28,7 @@ function LeaveGame({ leaveGame, buttonContent }) {
 LeaveGame.propTypes = {
     leaveGame: PropTypes.func.isRequired,
     buttonContent: PropTypes.string.isRequired,
+    width: PropTypes.string.isRequired,
 };
 
 export default LeaveGame;
