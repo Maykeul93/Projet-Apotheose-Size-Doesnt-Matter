966154723d0500008a70f99bf45f82f1a197d131
ArnaudG19 <arnaud.gueresse@gmail.com>
2021-04-28 18:13:12 +0200

    AboutUs Page

--- front/src/components/AboutUs/index.js
+++ front/src/components/AboutUs/index.js
@@ -6,22 +6,19 @@ function AboutUs() {
         <main className="page__main">
             <h1 className="title">Qui sommes-nous ?</h1>
             <div className="aboutUs">
-                <div>
-                    <div className="flexRight aboutUs__pictures">
-                    <img src="https://static.hitek.fr/img/actualite/chef-meurt-mordu-tete-serpent2.jpg"  alt="test"/>
-                    </div>
-                    <h2 className="flexRight aboutUs__name">Arnaud G</h2>
-                    <h3 className="flexRight aboutUs__type">Product Owner & Lead Dev Back</h3>
-                </div>
-                
-                <p className="aboutUs__description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum ex    sed    mi auctor, eget maximus neque lacinia. Phasellus cursus maximus odio,    nec  dapibus metus accumsan ac. Cras a dui cursus ex mollis vulputate. Donec    ac dui   vel lectus imperdiet sagittis sed ut massa. Nam nec erat quis nulla    aliquet   finibus. Proin at urna commodo, euismod nisl sit amet, maximus    est. Suspendisse   vel maximus ex. Cras accumsan dignissim tempus.
-                </p>
                 <div className="aboutUs__pictures">
                     <img src="https://static.hitek.fr/img/actualite/chef-meurt-mordu-tete-serpent2.jpg" />
                 </div>
-                <h2 className="aboutUs__name">Nicolas M</h2>
-                <h3 className="aboutUs__type">Lead Dev Front</h3>
+                <h2 className="aboutUs__name">Arnaud G</h2>
+                <h3 className="aboutUs__type">Product Owner & Lead Dev Back</h3>
+                <p className="aboutUs__description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum ex    sed    mi auctor, eget maximus neque lacinia. Phasellus cursus maximus odio,    nec  dapibus metus accumsan ac. Cras a dui cursus ex mollis vulputate. Donec    ac dui   vel lectus imperdiet sagittis sed ut massa. Nam nec erat quis nulla    aliquet   finibus. Proin at urna commodo, euismod nisl sit amet, maximus    est. Suspendisse   vel maximus ex. Cras accumsan dignissim tempus.
+                </p>
+                <div className="aboutUs__pictures imgright">
+                    <img src="https://static.hitek.fr/img/actualite/chef-meurt-mordu-tete-serpent2.jpg" />
+                </div>
+                <h2 className="aboutUs__name right">Nicolas M</h2>
+                <h3 className="aboutUs__type right">Lead Dev Front</h3>
                 <p className="aboutUs__description">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum ex sed    mi auctor, eget maximus neque lacinia. Phasellus cursus maximus odio, nec  dapibus metus accumsan ac. Cras a dui cursus ex mollis vulputate. Donec ac dui   vel lectus imperdiet sagittis sed ut massa. Nam nec erat quis nulla aliquet   finibus. Proin at urna commodo, euismod nisl sit amet, maximus est. Suspendisse   vel maximus ex. Cras accumsan dignissim tempus.
                 </p>
@@ -36,8 +33,8 @@ function AboutUs() {
                 <div className="aboutUs__pictures">
                     <img src="https://static.hitek.fr/img/actualite/chef-meurt-mordu-tete-serpent2.jpg" />
                 </div>
-                <h2 className="aboutUs__name">Alexandre B</h2>
-                <h3 className="aboutUs__type">Git Master & Dev Back</h3>
+                <h2 className="aboutUs__name right">Alexandre B</h2>
+                <h3 className="aboutUs__type right">Git Master & Dev Back</h3>
                 <p className="aboutUs__description">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum ex sed    mi auctor, eget maximus neque lacinia. Phasellus cursus maximus odio, nec  dapibus metus accumsan ac. Cras a dui cursus ex mollis vulputate. Donec ac dui   vel lectus imperdiet sagittis sed ut massa. Nam nec erat quis nulla aliquet   finibus. Proin at urna commodo, euismod nisl sit amet, maximus est. Suspendisse   vel maximus ex. Cras accumsan dignissim tempus.
                 </p>
