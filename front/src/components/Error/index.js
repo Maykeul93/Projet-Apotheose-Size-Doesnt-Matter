f695145812d49ffc7a625c9e1c037db3ff2e1255
nmelling <nicolas.mellinger1@gmail.com>
2021-04-29 09:45:32 +0200

    Creation & Connection of Error Component

---
+++ front/src/components/Error/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+function Error() {
+    return (
+        <div className="error">Error</div>
+    );
+}
+
+Error.propTypes = {
+
+};
+
+export default Error;
