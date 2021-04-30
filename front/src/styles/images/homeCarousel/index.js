88049041afc7b99290f412f32f6adbf3da6e5e47
ArnaudG19 <arnaud.gueresse@gmail.com>
2021-04-30 10:06:59 +0200

    Caroussel

--- front/src/styles/images/homeCarousel/index.js
+++ front/src/styles/images/homeCarousel/index.js
@@ -1,17 +1,23 @@
 import img1 from './img1.jpg'
-import img2 from './img2.jpg'
-import img3 from './img3.jpg'
-import img4 from './img4.jpg'
-import img5 from './img5.jpg'
-import img6 from './img6.jpg'
+// import img2 from './img2.jpg'
+// import img3 from './img3.jpg'
+// import img4 from './img4.jpg'
+// import img5 from './img5.jpg'
+// import img6 from './img6.jpg'
+import home from './home.png'
+import salon from './salon.png'
+import room from './room.png'
+import game from './game.png'
+import profil from './profil.png'
+import historique from './historique.png'
 
 const images = [
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6,
+    home,
+    salon,
+    room,
+    game, 
+    profil, 
+    historique, 
 ]
 
 export default images;
